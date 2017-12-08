ae2d44ece0be12b5b3fa15713e19e5d41b5611e4
papachen <447334358@qq.com>
2017-12-08 10:47:51 +0800

    'chen'

--- src/utils/event.js
+++ src/utils/event.js
@@ -11,30 +11,34 @@ class Event {
     this._listener = {}
   }
 
-  $emit(name, args) {
+  $emit(name) {
+    if (typeof name === 'undefined') return this
     if (typeof this._listener[name] === 'undefined') {
-      this._register(name)
+      _register.apply(this, name)
     }
     for (let i = 0; i < this._listener[name].length; i++) {
       if (typeof this._listener[name][i] === 'function') {
-        this._listener[name][i](args)
+        this._listener[name][i].apply(this, arguments.slice(1))
       }
     }
+    return this
   }
 
   $on(name, fn) {
     if (typeof this._listener[name] === 'undefined') {
-      this._register(name)
+      _register.apply(this, name)
     }
     for (let i = 0; i < this._listener[name].length; i++) {
       if (this._listener[name] === fn) {
-        return 'Event already exists'
+        return this
       }
     }
     this._listener[name].push(fn)
+    return this
   }
 
   $off(name, fn) {
+    if (typeof name === 'undefined') return this
     if (this._listener[name]) {
       if (typeof fn === 'function') {
         for (let i = 0; i < this._listener[name].length; i++) {
@@ -47,11 +51,12 @@ class Event {
         this._listener[name] = []
       }
     }
+    return this
   }
+}
 
-  _register(name) {
-    this._listener[name] = []
-  }
+function _register(name) {
+  this._listener[name] = []
 }
 
 export default new Event()
